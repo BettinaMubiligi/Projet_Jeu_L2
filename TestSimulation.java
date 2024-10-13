ad2ddfa2339dbe09928e435a6bff8e462237f2e8
BettinaMubiligi <155474592+BettinaMubiligi@users.noreply.github.com>
2024-10-13 19:22:22 +0200

    Add files via upload

--- TestSimulation.java
+++ TestSimulation.java
@@ -2,34 +2,28 @@
 
 import java.util.ArrayList;
 
-public class TestSimulation{
-    public static void main(String[] args){
-        //Simulation 
-       ArrayList<Contenu> c = new ArrayList<Contenu>();
-
+    public class TestSimulation{
+        public static void main(String[] args){
+        //Simulation
+        ArrayList<Contenu> c = new ArrayList<Contenu>();
         Grille g = new Grille(6,5);
-        Agent4 agent =new Agent4(g);        
-
-        for(int i = 0; i < 15; i++){
-            c.add(new Diamant(1));
+        Agent4 agent =new Agent4(g);
+        for(int i = 0; i < 5; i++){
+        c.add(new Diamant(1));
         }
-
-        for(int i = 0; i < 2; i++){
-            c.add(new Gardien("Gardien", 1));
+        for(int i = 0; i < 5; i++){
+        c.add(new Gardien("Gardien", 1));
         }
-
-        for(int i = 0; i < 3; i++){
-            c.add(new GardienTeleportable("Gardien", 1, g));
+        for(int i = 0; i < 20; i++){
+        c.add(new GardienTeleportable("Gardien", 1, g));
         }
-
-        Simulation s = new Simulation(agent, g, c, 27);
+        Simulation s = new Simulation(agent, g, c, 30);
+        System.out.println(g);
         System.out.println(s);
         s.lance(10);
         System.out.println(s);
 
 
     }
-
-
 
 }
